5293d06a40c60826605fe58a8d8a3942da69bdca
golismero96 <golismero96@gmail.com>
2024-08-06 10:20:06 +0330

    update to my informations

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,9 +2,8 @@ import close from "./close.svg";
 import logo from "./logo.svg";
 import menu from "./menu.svg";
 
-import algorithms from "./algorithms.png";
-import devnotes from "./devnotes.png";
-import oscs from "./oscs.png";
+import cdnPanel from "./cdnPanel.png";
+import ziggurat from "./ziggurat.png";
+import portal from "./portal.png";
 
-export { algorithms, close, devnotes, logo, menu, oscs };
-
+export { cdnPanel, close, logo, menu, portal, ziggurat };
--- src/data/index.js
+++ src/data/index.js
@@ -1,8 +1,4 @@
-import {
-  algorithms,
-  devnotes,
-  oscs,
-} from "../assets";
+import { cdnPanel, ziggurat, portal } from "../assets";
 
 export const navLinks = [
   {
@@ -25,67 +21,59 @@ export const navLinks = [
 
 const experiences = [
   {
-    title: "YouTube Content Creator",
+    title: "Ziggurat",
     company_name: "Self Employed",
-    date: "2016 - Present",
+    date: "2024 - Present",
     details: [
-      "Built a subscriber base of over <span style='color: white;'>500,000 subscribers</span> by creating video content to help programmers.",
-      "Crafted visually appealling programming videos that have garnered over <span style='color: white;'>30,000,000 views</span>.",
-      "Produced high-quality educational and entertaining videos for clients including <span style='color: white;'>Intel, JetBrains, and MicroCenter</span>.",
+      "Building a large project using Nx technology to manage an admin panel, shopping website and its back-end",
     ],
   },
   {
-    title: "Software Developer",
-    company_name: "Indie",
-    date: "2019 - 2023",
+    title: "Front End Developer",
+    company_name: "GreenPlus",
+    date: "2021 - Present",
     details: [
-      "Developed and delivered custom interdisciplinary coding portfolio for clients including <span style='color: white;'>Nvidia, Hostinger, and Amazon</span>.",
-      "<span style='color: white;'>Designed and developed innovative</span> AI applications and interactive websites.",
-      "<span style='color: white;'>Managed full project lifecycle</span> from concept to deployment in successful and timely project completions.",
+      "As Front and Developer, I have collaborated in building a large management panel in the field of cloud infrastructure, CDN, Blockchain, Kubernetes that has more than 5,000 customers.",
+      "- I worked more than 3 years only as a front-end developer.",
+      "- I improved the quality of the panel by updating essential packages, tree shaking, purgecss and making the program reusable.",
+      "- Features such as dark mode and multilingual were added to the panel. I used Sentry for debugging.",
+      "- For state management, context, redux, redux-toolkit, as well as Axios and RTK-query were used to send requests to the backend.",
+      "- Keycloak was used for authentication, which required running the project with Docker.",
     ],
   },
   {
-    title: "Software Engineer",
-    company_name: "Prime 3",
-    date: "2018 - 2019",
+    title: "Junior Developer",
+    company_name: "GreenWeb",
+    date: "2020 - 2021",
     details: [
-      "Built custom enterprise applications for a <span style='color: white;'>Fortune 500 company</span> as a full-stack software engineer.",
-      "Developed and maintained <span style='color: white;'>scalable backend services</span>, ensuring high availability for critical business applications.",
-      "<span style='color: white;'>Collaborated with a team</span> to design and implement front-end interfaces.",
-    ],
-  },
-  {
-    title: "Computer Science",
-    company_name: "ODU",
-    date: "2015 - 2018",
-    details: [
-      "Built a <span style='color: white;'>computer science foundation</span> learning theory, computer architecture, and software engineering.",
-      "Worked and interned at <span style='color: white;'>NASA and Norfolk Southern Railway</span> to gain practical experience in the field of data analysis.",
-      "Acted as a member of the <span style='color: white;'>Association for Computing Machinery</span> (ACM).",
+      "As Junior Developer, I have collaborated in building a management panel in the field of infrastructure CDN that has more than 3000 customers.",
+      "- I joined the CDN team as a rookie developer.",
+      "- I spent 3 months learning and researching Content Distribution Networks (CDN).",
+      "- Together with a senior developer, I developed the management panel and its APIs to serve other team members and partner companies.",
+      "- For 6 months, I used react and Django to develop the panel.",
     ],
   },
 ];
 
 const portfolio = [
   {
-    name: "Open Source Computer Science Repo",
+    name: "Front End Developer",
     description:
-      "A GitHub repo with over 17,000 stars containing a curated list of free online courses from reputable universities that satisfy undergraduate computer science requirements.",
-    image: oscs,
+      "As Front and Developer, I have collaborated in building a large management panel in the field of cloud infrastructure, CDN, Blockchain, Kubernetes that has more than 5,000 customers.",
+    image: portal,
   },
   {
-    name: "Dev Notes",
+    name: "Ziggurat",
     description:
-      "A newsletter with over 6,000 readers made for software developers to keep up with this rapidly evolving industry, with a sister platform in progress.",
-    image: devnotes,
+      "Building a large project using Nx technology to manage an admin panel, shopping website and its back-end",
+    image: ziggurat,
   },
   {
-    name: "Visually Understanding Algorithms",
+    name: "Junior Developer",
     description:
-      "A showcase of animated algorithms coded using TypeScript, with the video garnering over 400,000 views.",
-    image: algorithms,
+      "As Junior Developer, I have collaborated in building a management panel in the field of infrastructure CDN that has more than 3000 customers.",
+    image: cdnPanel,
   },
 ];
 
 export { experiences, portfolio };
-
